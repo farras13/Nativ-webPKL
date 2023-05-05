5ade4226fab29d049572eb8071b8d10a3f361ab4
farras13 <farrasgopal13@gmail.com>
2023-05-05 18:37:10 +0700

    change db

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -1,7 +1,9 @@
 <?php
 $db_host = "localhost";
-$db_user = "pkljtimy_tiayuni";
-$db_pass = "tiawahyunirose123";
+// $db_user = "pkljtimy_tiayuni";
+$db_user = "root";
+$db_pass = "";
+// $db_pass = "tiawahyunirose123";
 $db_name = "pkljtimy_db_si_pkl";
  
 $koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
